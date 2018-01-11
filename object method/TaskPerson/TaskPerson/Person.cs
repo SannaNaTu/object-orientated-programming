76eebf51039397afee1d96661842de8f14cac3c1
SannaNaTu <korhonsm@gmail.com>
2018-01-11 11:02:14 +0200

    taskperson

---
+++ object method/TaskPerson/TaskPerson/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskPerson
+{
+    class Person
+    {
+        //Fields
+        public string Name;
+        private int age;
+
+        //Constuctor
+        public Person()
+        {
+            Name = "unknown";
+            age = 0;
+        }
+        public Person(string name, int age)
+        {
+            Name = name;
+            this.age = age;
+        }
+        //Methods
+        public void GrowOld()
+        {
+            this.age++;
+        }
+        public void PrintPersonInfo()
+        {
+            Console.WriteLine($"Nimi: {Name}\nIkä: {this.age}\nAikuinen:{IsAdult()}");
+        }
+        public bool IsAdult()
+        {
+            return this.age >= 18;
+            //alla oleva tekee saman kuin ylläoleva lauseke
+            //if (this.age >= 18)
+            //    return true;
+            //else
+            //return false;
+                
+        }
+        public int GetAge()
+        {
+            return this.age;
+        }
+    }
+}
---
+++ object method/TaskPerson/TaskPerson/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TaskPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person personSanna = new Person();
+            personSanna.Name = "Sanna N";
+            personSanna.GrowOld();
+            personSanna.PrintPersonInfo();
+
+            Person newPerson = new Person(" Joe Doe", 17);
+            newPerson.PrintPersonInfo();
+            newPerson.GrowOld();
+            newPerson.PrintPersonInfo();
+            Console.ReadKey();
+
+        }
+    }
+}
