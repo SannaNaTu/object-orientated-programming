35335db429208e9b3d4a2edde1e9cf2e7909f122
SannaNaTu <korhonsm@gmail.com>
2018-01-15 09:04:03 +0200

    book task

---
+++ object method/TaskPerson/Book task/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Book task
+{
+    class Book
+{
+    //Fields 
+
+    public string Title;
+    public string Author;
+    public int Id;
+    public int Price;
+
+    //Constuctor
+    public Book()
+    {
+
+    }
+    //Methods
+}
+}
---
+++ object method/TaskPerson/Book task/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Book_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
