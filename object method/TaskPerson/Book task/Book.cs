c086b57e5b653101f9680c5a48e12b84a51aa903
SannaNaTu <korhonsm@gmail.com>
2018-01-17 19:13:45 +0200

    book task

--- object method/TaskPerson/Book task/Book.cs
+++ object method/TaskPerson/Book task/Book.cs
@@ -2,22 +2,53 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Book task
+namespace BookTask
 {
+    // Luokka = class, nimi kirjoitetaan aina Isolla
     class Book
-{
-    //Fields 
-
-    public string Title;
-    public string Author;
-    public int Id;
-    public int Price;
-
-    //Constuctor
-    public Book()
     {
+        //Fields  (kirjassa attribuutit,olioiden ominaisuuksia, jäsenmuuttujat)
+        // public, näkyy kaikille / private, näkyy vain luokassa
+        // jäsenmuuttujat  ja jäsenfunktiot 
+        public string Title;
+        public string Author;
+        public int Id;
+        public double Price;
 
-    }
-    //Methods
+        //Constuctor =muodostin, muuttujille arvot
+        public Book()
+        {
+            Title = "Thinking in Java";
+            Author = "Bruce Eckel's";
+            Id = 95499;
+            Price = 46.70;
+        }
+        public Book(string title, string author, int id, double price)
+        {
+            Title = title;
+            Author = author;
+            Id = id;
+            Price = price;
+
+        }
+        //Methods ( olioiden toiminnat, tehtävät, käyttötarkoitukset)
+        public void PrintBookInfo()
+        {
+            Console.WriteLine($"Nimi: {Title}\nKirjoittaja: {Author}\nId-numero: {Id}\nHinta: {Price}€");
+        }
+        //    //this. = operaattori viittaa käsittelyssä olevaan olioon eli siihen, jonka metodeja käytetään
+        //}
+
+            // Tämä Jounin mallista
+        public string CompareBook(Book book)
+        {
+            if (this.Price > book.Price)
+                return $"{this.Title} on kalliimpi kuin {book.Title} kirja";
+            else if (this.Price < book.Price)
+                return $"{book.Title} on kalliimpi kuin {this.Title} kirja";
+            else
+                return $"Hinnat ovat yhtä suuret";
+
 }
+    }
 }
--- object method/TaskPerson/Book task/Program.cs
+++ object method/TaskPerson/Book task/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 
-namespace Book_task
+namespace BookTask
 {
     class Program
+
     {
+        //Sanna kotsassa ihmettelee. Vähän sekava kun mukana muistiinpanoja!
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Book book = new Book(); // new:llä luodaan aina uusi olio, object,instanssi,Muodostin funktiota kutsutaan instanssin muodostuksen yhteydessä new-lauseella
+            book.PrintBookInfo(); // tulostaa kirjan tiedot
+            Book newBook = new Book("C#- Ohjelmointi", "Ghodrat Moghadampour", 97895, 29.90);
+            newBook.PrintBookInfo(); // tulostaa uuden kirjan tiedot
+            Console.WriteLine(newBook.CompareBook(book)); //vertaa hintoja
+            Console.ReadKey();
+
         }
     }
 }
