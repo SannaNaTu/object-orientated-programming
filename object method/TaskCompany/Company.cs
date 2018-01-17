98b1a8348a3bcf4772a3ff1c1e73d0610272776a
SannaNaTu <korhonsm@gmail.com>
2018-01-17 20:00:45 +0200

    task company

---
+++ object method/TaskCompany/Company.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskCompany
+{
+    class Company
+    {
+        public string Title;
+        public string Address;
+        public int Phone;
+        public double Outcome;
+        public double Exspense;
+    }
+}
