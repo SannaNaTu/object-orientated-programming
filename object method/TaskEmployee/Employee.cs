37037135f3551a0dadc0a63a94031c9dea1a1045
SannaNaTu <korhonsm@gmail.com>
2018-01-17 20:04:07 +0200

    task company

---
+++ object method/TaskEmployee/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskEmployee
+{
+    class Employee
+    {
+        public string Name;
+        public int Id;
+        public string Position;
+        public double Salary;
+    }
+}
---
+++ object method/TaskEmployee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
