3e4e37d748f495990238c8bebaf1f1799a26bd7b
SannaNaTu <korhonsm@gmail.com>
2018-01-19 20:47:43 +0200

    Company fixes

--- object method/TaskCompany/Company.cs
+++ object method/TaskCompany/Company.cs
@@ -10,54 +10,61 @@ namespace TaskCompany
         public string Title;
         public string Address;
         public string Phone;
-        public double Outcome;
+        public double Income;
         public double Expense;
 
         //Constuctor =muodostin //tehtävä oletusmuodostin (parametritön) eli ei palauta mitään arvoa
         public Company()
         {
-            Title = "Pikku Firma";
-            Address = "Pikkufirmantie";
-            Phone = "050123456";
-            Outcome = 10000;
-            Expense = 5000;
+            Title = "-";
+            Address = "-";
+            Phone = "-";
+            Income = 0;
+            Expense = 0;
 
         }
         //Constuctor =muodostin //tehtävä muodostin, joka sisältää kaikki parametrit
-        public Company(string Title,string Address,string Phone, double Outcome, double Expense)
+        public Company(string title,string address,string phone, double income, double expense)
         {
+            Title = title;
+            Address = address;
+            Phone = phone;
+            Income = income;
+            Expense = expense;
 
         }
         //Constuctor =muodostin //tehtävä kopiontimuodostin, jolla olio voidaan kopioida. 
 
-            public Company(Company CopyCompany)
+        public Company(Company CopyCompany)
         {
             Title = CopyCompany.Title;
             Address = CopyCompany.Address;
             Phone = CopyCompany.Phone;
-            Outcome = CopyCompany.Outcome;
+            Income= CopyCompany.Income;
             Expense = CopyCompany.Expense;
         }
 
         public void PrintCompanyInfo()
         {
             Console.WriteLine($"Firman tiedot:");
-            Console.WriteLine($"Nimi: {Title}\nOsoite: {Address}\nPuhelinnumero: {Phone}\nTulot: {Outcome:F}€\nTulot: {Expense:F}€");
+            Console.WriteLine($"Nimi: {Title}\nOsoite: {Address}\nPuhelinnumero: {Phone}\nTulot: {Income:F}€\nTulot: {Expense:F}€\n");
         }
         //Methods   
-        //Määritä luokalle myös metodi, joka tulojen ja menojen perusteella laskee firman voiton. 
-        //Metodin pitää laskea voitto%=[(tulot-menot)/menot x 100] ja sen perusteella tulostaa tulostaa näytölle viesti, 
-        //joka kertoo, kuinka hyvin firmalla menee.
-        public void CompanyProfit ()
+        public string Profit(Company company)
         {
-            if ( )
-
+            double profit = (company.Income - company.Expense) / Expense*100;
+            if (profit < 100)
+            {
+                return "Yhtiöllä menee kehnosti!\n";
+            }
+            else if (profit > 100 && profit < 300) // Miksi ei tämä osio tulostu! 
+            {
+                return "Yhtiöllä menee ihan ok!\n";
+            }
+            else 
+            {
+                return "Yhtiöllä menee super hyvin!\n";
+            }
         }
     }
-    //1kehnosti, jos voitto on alle 100% suurempi kuin menot
-    //2tyydyttävästi, jos voitto on vähintään 200% suurempi kuin menot
-    //3hyvin, jos voitto on vähintään 300% suurempi kuin menot
-    //Luo testausohjelmassa oliota Firma-luokasta ja kopioi yksi olio kopiointimuodostimella.
-    //Luotuasi olioita, kutsu niiden metodi, joka kertoo firman tilanteesta.
-
 }
--- object method/TaskCompany/Program.cs
+++ object method/TaskCompany/Program.cs
@@ -6,7 +6,27 @@ namespace TaskCompany
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Company company = new Company("Pikku Firma", "Pikkukatu","050123123",40000,20000 );
+            Company company1 = new Company(company);
+            Company company2 = new Company("Iso Firma", "Isokatu", "-", 1000000, 1000000);
+            Company company3 = new Company(company2);
+            company3.Income = 640000;
+            company3.Expense = 320000;
+
+            company.PrintCompanyInfo();
+            Console.WriteLine(company.Profit(company));
+            company1.PrintCompanyInfo();
+            Console.WriteLine(company1.Profit(company1));
+            company2.PrintCompanyInfo();
+            Console.WriteLine(company2.Profit(company2));
+            company3.PrintCompanyInfo();
+            Console.WriteLine(company3.Profit(company3));
+
+            Console.ReadKey();
+
+
         }
     }
 }
