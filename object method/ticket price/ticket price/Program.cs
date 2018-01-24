d6022fcc6a616f9dcd74c34ff52655dabea479bd
SannaNaTu <korhonsm@gmail.com>
2018-01-24 10:52:15 +0200

    ticket task

--- object method/ticket price/ticket price/Program.cs
+++ object method/ticket price/ticket price/Program.cs
@@ -11,6 +11,7 @@ namespace TicketPrice
             Ticket ticket = new Ticket();
             ticket.AskInfo();
             ticket.TicketPrice();
+            Console.WriteLine("Kiitos, osallistumisesta!");
             Console.ReadKey();
 
         }
--- object method/ticket price/ticket price/Ticket.cs
+++ object method/ticket price/ticket price/Ticket.cs
@@ -8,20 +8,7 @@ namespace TicketPrice
     {
         public string Name;
         public int Age;
-        //public string Conscript; // varusmies
-        //public string Mtk; //MTK-jäsen
-        //public string Student;
 
-
-        //public Ticket(string name, int age) //, string conscript, string mtk, string student
-        //{
-        //    Name = name;
-        //    Age = age;
-        //    //Conscript = conscript;
-        //    //Mtk = mtk;
-        //    //Student = student;
-
-        //}
             public void AskInfo()
             {
                 Console.Write("Anna nimesi: "); // Kysytään nimi
@@ -30,34 +17,65 @@ namespace TicketPrice
                 Age = int.Parse(Console.ReadLine());
             }
 
-            public void TicketPrice()
+        public void TicketPrice()
+        {
+            float discount = 0;
+            float price = 16;
+            if (Age < 7)
             {
-                float discount = 0;
-                float price = 16;
-                if (Age < 7)
-                {
-                    discount = 100;
-                    Console.WriteLine($"Hei, {Name} lippusi hinta on: {price - (price * discount / 100)}€");
-                    Console.ReadLine();
-                }
-                else if (Age >= 7 && Age <= 15)
-                {
-                    discount = 50;
-                    Console.WriteLine($"Hei, {Name} lippusi hinta on: {price - (price * discount / 100)}€");
-                    Console.ReadLine();
-                }
-                else if (Age >= 65)
-                {
-                    discount = 100;
-                    Console.WriteLine($"Hei, {Name} lippusi hinta on: {price - (price * discount / 100)}€");
-                    Console.ReadLine();
-                }
-                else if (Age > 15 && Age < 65)
-                {
-
-                }
+                discount = 100;
+                Console.WriteLine($"Hei, {Name} lippusi hinta on: {price - (price * discount / 100)}€");
+                Console.ReadLine();
+            }
+            else if (Age >= 7 && Age <= 15)
+            {
+                discount = 50;
+                Console.WriteLine($"Hei, {Name} lippusi hinta on: {price - (price * discount / 100)}€");
 
             }
+            else if (Age >= 65)
+            {
+                discount = 100;
+                Console.WriteLine($"Hei, {Name} lippusi hinta on: {price - (price * discount / 100)}€");
+
+            }
+            else if (Age > 15 && Age < 65)
+            {
+                Console.Write("Oletko varusmies? Y/N: ");
+                string conscriptResponse = Console.ReadLine().ToUpper();
+                if (conscriptResponse == "Y")
+                {
+                    discount = 50;
+
+                }
+                else
+                {
+                    Console.Write("Oletko MTK:n jäsen? Y/N:");
+                    string mtkresponse = Console.ReadLine().ToUpper();
+                    if (mtkresponse == "Y")
+                    {
+                        discount = 15;
+
+                    }
+                    Console.Write("Oletko opiskelija? Y/N:");
+                    string studentresponse = Console.ReadLine().ToUpper();
+                    if ( studentresponse == "Y")
+                    {
+                        discount = discount + 45;
+
+                    }
+                }
+                Console.WriteLine($"Hei, {Name} lippusi hinta on: {price - (price * discount / 100)}€");
+
+            }
+
+            }
+
+        }
+           
+                
+
+            
         
-    }
+    
 }
