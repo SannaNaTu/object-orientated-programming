b50b584da32314974ce9fa5969d254730e10a150
SannaNaTu <korhonsm@gmail.com>
2018-01-24 10:31:11 +0200

    ticket price

---
+++ object method/ticket price/ticket price/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TicketPrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Maatalousnäyttelyn lipunhinta");
+            Ticket ticket = new Ticket();
+            ticket.AskInfo();
+            ticket.TicketPrice();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ object method/ticket price/ticket price/Ticket.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketPrice
+{
+    class Ticket
+    {
+        public string Name;
+        public int Age;
+        //public string Conscript; // varusmies
+        //public string Mtk; //MTK-jäsen
+        //public string Student;
+
+
+        //public Ticket(string name, int age) //, string conscript, string mtk, string student
+        //{
+        //    Name = name;
+        //    Age = age;
+        //    //Conscript = conscript;
+        //    //Mtk = mtk;
+        //    //Student = student;
+
+        //}
+            public void AskInfo()
+            {
+                Console.Write("Anna nimesi: "); // Kysytään nimi
+                Name = Console.ReadLine();
+                Console.Write("Anna ikäsi: "); // Kysytään ikä
+                Age = int.Parse(Console.ReadLine());
+            }
+
+            public void TicketPrice()
+            {
+                float discount = 0;
+                float price = 16;
+                if (Age < 7)
+                {
+                    discount = 100;
+                    Console.WriteLine($"Hei, {Name} lippusi hinta on: {price - (price * discount / 100)}€");
+                    Console.ReadLine();
+                }
+                else if (Age >= 7 && Age <= 15)
+                {
+                    discount = 50;
+                    Console.WriteLine($"Hei, {Name} lippusi hinta on: {price - (price * discount / 100)}€");
+                    Console.ReadLine();
+                }
+                else if (Age >= 65)
+                {
+                    discount = 100;
+                    Console.WriteLine($"Hei, {Name} lippusi hinta on: {price - (price * discount / 100)}€");
+                    Console.ReadLine();
+                }
+                else if (Age > 15 && Age < 65)
+                {
+
+                }
+
+            }
+        
+    }
+}
