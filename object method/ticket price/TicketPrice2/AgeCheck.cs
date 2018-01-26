6dbb1f38972eb38910abae68a07414c893f5e227
SannaNaTu <korhonsm@gmail.com>
2018-01-26 08:56:34 +0200

    ticket task 2.0

---
+++ object method/ticket price/TicketPrice2/AgeCheck.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketPrice2
+{
+    class AgeCheck
+    {
+    }
+}
---
+++ object method/ticket price/TicketPrice2/YesNo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketPrice2
+{
+    class YesNo
+    {
+        private static bool YesNou (string yesnocheck)
+        {
+            while (true)
+            {
+                Console.Write("{0} Y/N : ", yesnocheck);
+                char c = char.ToUpper(Console.ReadKey().KeyChar);
+                Console.WriteLine();
+                if(c == 'Y')
+                {
+                    return true;
+                }
+                if (c =='N')
+                {
+                    return false;
+                }
+                Console.WriteLine("Vastaa Y (Kyllä) tai N (Ei)");
+            }
+        }
+    }
+}
