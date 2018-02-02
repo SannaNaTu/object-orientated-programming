7f600588d6d35aa91f4b4df8f56ff206d7dab07c
SannaNaTu <korhonsm@gmail.com>
2018-02-02 15:05:21 +0200

    Booknwriter task fixes

--- object method/BookNWriter/BookNWriter/Book.cs
+++ object method/BookNWriter/BookNWriter/Book.cs
@@ -4,105 +4,112 @@ using System.Text;
 
 namespace BookNWriter
 {
-    class Book //Luokka
+    namespace Kirjallisuus
     {
-        //Fields, atribuutit, ( privaatit siksi _-viiva)
-        string _name;
-        readonly string _author;
-        double _price;
 
-        static string _bookTheme;
 
-        public string Name { get => _name; set => _name = value; }
-        public static string BookTheme { get => _bookTheme; set => _bookTheme = value; }
-        public double Price
+        class Book //Luokka
         {
-            get => _price;
+            //Fields, atribuutit, ( privaatit siksi _-viiva)
+            string _name;
+            readonly string _author;
+            double _price;
 
-            set
+            static string _bookTheme;
+            //Encapsulated properties
+            public string Name { get => _name; set => _name = value; }
+            public static string BookTheme { get => _bookTheme; set => _bookTheme = value; }
+            public double Price
             {
-                _price = value;
-                if (value >= 30)
+                get => _price;
+
+                set
                 {
-                    _price *= 0.9; // 10% discount 
+                    _price = value;
+                    if (value >= 30)
+                    {
+                        _price *= 0.9; // 10% discount 
+                    }
                 }
             }
-        }
 
-        //Muodostin
-        public Book()
-        {
-            Name = "Kirja 1";
-            _author= "Kirjamies Kimmo";
-            Price = 10;
-            BookTheme = "Fantasia";
-        }
-        public Book(string name, string author, double price, string bookTheme)
-        {
-            Name = name;
-            _author = author;
-            Price = price;
-            BookTheme = bookTheme;
-        }
-        //Metodit
-        public string GetBook(Book book)
+            //Muodostin
+            public Book()
+            {
+                Name = "Kirja 1";
+                _author = "Kirjamies Kimmo";
+                Price = 10;
+                BookTheme = "Fantasia";
+            }
+            public Book(string name, string author, double price, string bookTheme)
+            {
+                Name = name;
+                _author = author;
+                Price = price;
+                BookTheme = bookTheme;
+            }
+            //Metodit
+            public string GetBook(Book book)
 
-        {
+            {
 
-            return $"------------------\n" +
-                   $"Kirja: {Name}\n" +            
-                   $"Hinta: {Price}\n" +
-                   $"Teema: {BookTheme}\n";
+                return $"------------------\n" +
+                       $"Kirja: {Name}\n" +
+                       $"Hinta: {Price}\n" +
+                       $"Teema: {BookTheme}\n";
 
-        }
-        public string GetBook()
-        {
-            return $"------------------\n" +
-                   $"Kirja: {Name}\n" +
-                   $"Hinta: {Price}\n" +
-                   $"Teema: {BookTheme}\n";
-        }
-        public string CompareBook(string theme)
-        {
-            if (theme == Name)
-                return $"{theme} Löytyy valikoimasta!";
-            else 
+            }
+            public string GetBook()
+            {
+                return $"------------------\n" +
+                       $"Kirja: {Name}\n" +
+                       $"Hinta: {Price}\n" +
+                       $"Teema: {BookTheme}\n";
+            }
+            public string CompareBook(string theme)
+            {
+                if (theme == Name)
+                    return $"{theme} Löytyy valikoimasta!";
+                else
                     return $"{theme} Ei löydy valikoimasta!";
 
+            }
+
+
         }
-        
-
-    }
-
-    class Author // Uusi luokka 
-    {
-        string _name;
-        string _bday;
-        Book bookXXX;
 
 
-        public Author(string name, string bday, Book book)
+
+
+        class Author // Uusi luokka 
         {
-            _name = name;
-            _bday = bday;
-            bookXXX = book;
+            string _name;
+            string _bday;
+            Book bookXXX;
+
+
+            public Author(string name, string bday, Book book)
+            {
+                _name = name;
+                _bday = bday;
+                bookXXX = book;
+
+            }
+            public Author()
+            {
+                _name = "Kirja Maija";
+                _bday = "01.05.1500";
+                bookXXX = new Book();
+
+            }
+            public string AuthorInfo()
+            {
+                return $"Nimi: {_name}" +
+                    $" ja syntymäpäivä: {_bday}\n" +
+                    $"Kirjopittanut kirjan:\n {bookXXX.GetBook()}";
+            }
+
 
         }
-        public Author()
-        {
-            _name = "Kirja Maija";
-            _bday = "01.05.1500";
-            bookXXX = new Book();
-
-        }
-        public string AuthorInfo()
-        {
-            return $"Nimi: {_name}" +
-                $" ja syntymäpäivä: {_bday}\n" +
-                $"Kirjopittanut kirjan:\n {bookXXX.GetBook()}";
-        }
-
-
-    }
-
+    } 
 }
--- object method/BookNWriter/BookNWriter/Program.cs
+++ object method/BookNWriter/BookNWriter/Program.cs
@@ -8,19 +8,22 @@ namespace BookNWriter
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            Book book = new Book();
-            Book.BookTheme = "xx";
+            Kirjallisuus.Book book = new Kirjallisuus.Book();
+            Kirjallisuus.Book.BookTheme = "xx";
             Console.WriteLine(book.GetBook(book));
             Console.WriteLine(book.CompareBook("Kirja 1"));
 
-            Book book2 = new Book("Testikirja", "Tellu",32,"Kauhu");
+            Kirjallisuus.Book book2 = new Kirjallisuus.Book("Testikirja", "Tellu",32,"Kauhu");
             Console.WriteLine(book2.GetBook(book));
             book2.Name = "Uusnimi";
-            Book.BookTheme = "Seikkailu";
+            Kirjallisuus.Book.BookTheme = "Seikkailu";
             Console.WriteLine(book2.CompareBook("Uusnimi"));
 
-            Author KirjailijaArttu = new Author("Arttu", "2.3.1978", book2);
+            Kirjallisuus.Author KirjailijaArttu = new Kirjallisuus.Author("Arttu", "2.3.1978", book2);
+            Kirjallisuus.Author Kirjailija1 = new Kirjallisuus.Author("Testi", "päivä", new Kirjallisuus.Book("testi","tatti",55,"kauhu"));
             Console.WriteLine(KirjailijaArttu.AuthorInfo());
+            Console.WriteLine(Kirjailija1.AuthorInfo());
+
             Console.ReadKey();
 
            
