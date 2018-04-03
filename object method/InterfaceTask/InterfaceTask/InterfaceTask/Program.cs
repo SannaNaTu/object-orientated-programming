660085e624da557453e3e5540ca4b3d848b196bf
SannaNaTu <korhonsm@gmail.com>
2018-04-03 09:31:46 +0300

    Interface task start 2

---
+++ object method/InterfaceTask/InterfaceTask/InterfaceTask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_task
+{
+    // Mitäpä tästä opimme. en ollu laittanu GIttiin tiedostoa ja jostain syystä filu oli viottunu koneella.. tässä mitä sain pelastettua. Joten duunailen muut sitten uudestaan ! 
+    class Program
+    {
+        static void Main(string[] args)
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            List<Product> product = new List<Product>();
+        product.Add(new Product("Tuote1", 32, 3));
+            product.Add(new Product("Tuote2", 52, 5));
+            product.Add(new Product("Tuote3", 76, 3));
+           
+
+            Console.WriteLine(product[0].GetProduct("Tuote1"));
+            Console.WriteLine(product[0].CountValue());
+
+       
+        
+    }
+}
