a05daf1c0907aa16d55c07d7870ad05d986bb569
SannaNaTu <korhonsm@gmail.com>
2018-04-04 13:30:45 +0300

    more coming

--- object method/InterfaceTask/InterfaceTask/InterfaceTask/ProductClass.cs
+++ object method/InterfaceTask/InterfaceTask/InterfaceTask/ProductClass.cs
@@ -4,7 +4,44 @@ using System.Text;
 
 namespace InterfaceTask
 {
-    class ProductClass
+    class Product : IProduct
     {
+        //Fields
+        public string Name;
+        public double Price;
+        public double Count;
+
+        //Constructor
+        public Product(string name, double price, double count)
+        {
+            Name = name;
+            Price = price;
+            Count = count;
+        }
+        //Override ToString
+        public override string ToString()
+        {
+            return ($"\n{Name}\n" +
+                $"{Price}\n" +
+                $"{Count}\n");
+        }
+
+        //Interface Methods
+        public string GetProduct(string name)
+        {
+
+            if (name == Name)
+            {
+                Console.WriteLine($"{name} löytyi! ");
+            }
+            else
+                return null;
+        }
+
+        public void CountValue()
+        {
+            double Total = Price * Count;
+            Console.WriteLine($"Ostosten yhteisarvo on: {Total}");
+        }
     }
 }
