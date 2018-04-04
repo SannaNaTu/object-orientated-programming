d31911005edcf28fb3ed4bb4b77c658ae7fba100
SannaNaTu <korhonsm@gmail.com>
2018-04-04 16:44:37 +0300

    more and more

--- object method/InterfaceTask/InterfaceTask/InterfaceTask/IProducts.cs
+++ object method/InterfaceTask/InterfaceTask/InterfaceTask/IProducts.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace InterfaceTask
+namespace TaskProductInterface
 {
     interface IProducts
     {
+        void AddProducts(List<Product> product);
+        void PrintProducts();
     }
 }
