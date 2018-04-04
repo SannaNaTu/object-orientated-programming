f57195301578feb816a164d236426053d43a4931
SannaNaTu <korhonsm@gmail.com>
2018-04-04 13:09:55 +0300

    Getting there,slowly

--- object method/InterfaceTask/InterfaceTask/InterfaceTask/Program.cs
+++ object method/InterfaceTask/InterfaceTask/InterfaceTask/Program.cs
@@ -4,23 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Interface_task
+namespace InterfaceTask
 {
-    // Mitäpä tästä opimme. en ollu laittanu GIttiin tiedostoa ja jostain syystä filu oli viottunu koneella.. tässä mitä sain pelastettua. Joten duunailen muut sitten uudestaan ! 
+    // 3.4 Mitäpä tästä opimme. en ollu laittanu GIttiin tiedostoa ja jostain syystä filu oli viottunu koneella.. tässä mitä sain pelastettua. Joten duunailen muut sitten uudestaan ! 
     class Program
     {
         static void Main(string[] args)
+        {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             List<Product> product = new List<Product>();
-        product.Add(new Product("Tuote1", 32, 3));
+            product.Add(new Product("Tuote1", 32, 3));
             product.Add(new Product("Tuote2", 52, 5));
             product.Add(new Product("Tuote3", 76, 3));
-           
+
 
             Console.WriteLine(product[0].GetProduct("Tuote1"));
             Console.WriteLine(product[0].CountValue());
 
-       
+        }
         
     }
 }
